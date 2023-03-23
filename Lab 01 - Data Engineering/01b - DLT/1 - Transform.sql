6c665109c66af047ed0743ed6e2a417ca66f83f4
agnaug <agnaug>
2023-03-23 23:29:52 +0000

    Remove weather tables

--- Lab 01 - Data Engineering/01b - DLT/1 - Transform.sql
+++ Lab 01 - Data Engineering/01b - DLT/1 - Transform.sql
@@ -15,7 +15,7 @@
 -- MAGIC 
 -- MAGIC Key: `current_user_id`
 -- MAGIC 
--- MAGIC You can get value by running the following python cell
+-- MAGIC You can get value by running the following python cell. Comment out `%python` cell as DLT does not accept both `%python` and  %sql   in one notebook.
 
 -- COMMAND ----------
 
@@ -39,7 +39,7 @@ COMMENT "Bronze sales table with all transactions"
 AS 
 SELECT * 
 FROM
-cloud_files( '/FileStore/tmp/${current_user_id}/datasets/sales/' , "json") 
+cloud_files('/FileStore/tmp/${current_user_id}/datasets/sales/', "json") 
 
 -- COMMAND ----------
 
@@ -60,23 +60,6 @@ COMMENT "CDC records for our products dataset"
 AS 
 SELECT * FROM 
 cloud_files('/FileStore/tmp/${current_user_id}/datasets/products_cdc/', "json") ;
-
--- COMMAND ----------
-
--- MAGIC %md
--- MAGIC 
--- MAGIC ## Weather data
--- MAGIC 
--- MAGIC We also have some data from weather API - it can be transformed using a different notebook, but we can also add it here.
-
--- COMMAND ----------
-
-CREATE STREAMING LIVE TABLE bronze_weather
-TBLPROPERTIES ("quality" = "bronze")
-COMMENT "Records from weather api"
-AS 
-SELECT * FROM 
-cloud_files('/FileStore/tmp/${current_user_id}/datasets/weather/', "json") ;
 
 -- COMMAND ----------
 
@@ -128,15 +111,15 @@ CREATE STREAMING LIVE TABLE silver_sales_clean (
 TBLPROPERTIES ("quality" = "silver")
 COMMENT "Silver table with clean transaction records" AS
   SELECT
-    id as id,
-    ts as ts,
-    store_id as store_id,
-    customer_id as customer_id,
-    store_id || "-" || cast(customer_id as string) as unique_customer_id,
-    order_source as order_source,
-    STATE as order_state,
-    sale_items as sale_items
-  from STREAM(live.bronze_sales)
+    id AS id,
+    ts AS ts,
+    store_id AS store_id,
+    customer_id AS customer_id,
+    store_id || "-" || cast(customer_id as string) AS unique_customer_id,
+    order_source AS order_source,
+    STATE AS order_state,
+    sale_items AS sale_items
+  FROM STREAM(live.bronze_sales)
 
 -- COMMAND ----------
 
@@ -147,8 +130,7 @@ APPLY CHANGES INTO LIVE.silver_sales
   FROM 
   stream(live.silver_sales_clean)
   KEYS (id)
-  SEQUENCE BY ts
-
+  SEQUENCE BY ts;
 
 -- COMMAND ----------
 
@@ -188,73 +170,6 @@ APPLY CHANGES INTO LIVE.silver_products
   COLUMNS  * EXCEPT (_change_type, _change_timestamp, _rescued_data)
   STORED AS SCD TYPE 2
 
-
--- COMMAND ----------
-
--- MAGIC %md
--- MAGIC 
--- MAGIC ### Silver Weather Tables
--- MAGIC 
--- MAGIC Weather data needs some cleanup to convert it to multiple rows for easier querying
-
--- COMMAND ----------
-
-CREATE STREAMING LIVE TABLE silver_weather_clean AS
-select
-  concat_ws(latitude, longitude, t.time) as pk,
-  latitude,
-  longitude,
-  timezone,
-  generationtime_ms,
-  t.time,
-  t.temperature_2m,
-  t.rain
-from
-  (
-    select
-      latitude,
-      longitude,
-      timezone,
-      generationtime_ms,
-      explode(
-        arrays_zip(hourly.time, hourly.temperature_2m, hourly.rain) 
-      ) as t
-    from
-      (
-        select
-          latitude,
-          longitude,
-          timezone,
-          generationtime_ms,
-          from_json(
-            hourly,
-            schema_of_json(
-              '{"rain": [0, 0.1, 0.1], "temperature_2m": [21.9, 21.6, 21], "time": ["2023-03-01T00:00", "2023-03-01T01:00", "2023-03-01T02:00"]}'
-            )
-          ) as hourly
-        from
-          STREAM(live.bronze_weather)
-      )
-  )
-
--- COMMAND ----------
-
--- MAGIC %md 
--- MAGIC 
--- MAGIC ## Hands On Task!
--- MAGIC 
--- MAGIC Finish the cell below to have a silver_weather table with unique and up to date records by using APPLY CHANGES INTO and using table `live.silver_weather_clean`
--- MAGIC 
--- MAGIC For this table we do not need to track history - keep it as SCD Type 1
-
--- COMMAND ----------
-
--- REMOVE COMMENT AND FINISH WRITING THIS SQL
-
--- CREATE OR REFRESH STREAMING LIVE TABLE silver_weather;
-
--- APPLY CHANGES INTO LIVE.silver_weather FROM 
-
 -- COMMAND ----------
 
 -- MAGIC %md 
@@ -271,11 +186,13 @@ from
 
 -- COMMAND ----------
 
-CREATE LIVE TABLE country_sales
-select l.country_code, count(distinct s.id) as number_of_sales
-from live.silver_sales s 
-  join live.silver_stores l on s.store_id = l.id
-group by l.country_code;
+CREATE LIVE TABLE country_sales AS
+SELECT 
+  l.country_code, 
+  count(distinct s.id) AS number_of_sales
+FROM live.silver_sales s 
+INNER JOIN live.silver_stores l ON s.store_id = l.id
+GROUP BY l.country_code;
 
 -- COMMAND ----------
 
